7acd4e3bbeff527e13837ce82a426fbbd2dbd29a
Teller15mlg <Teller15mlg@gmail.com>
2022-04-10 16:30:28 +0300

    collider fixes

---
+++ VikingWalker/Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    void Update()
+    {
+        float currentspeed = GetComponentInParent<CharacterController>().velocity.magnitude;
+        GetComponent<Animator>().SetFloat("Speed", currentspeed);
+        float random = Mathf.Sin(Time.time);
+        GetComponent<Animator>().SetFloat("Randomizer", random);
+    }
+}
