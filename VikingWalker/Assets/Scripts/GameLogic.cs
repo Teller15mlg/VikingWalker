25a77a40137e13eb022845da3ee77a6eee818af0
Teller15mlg <Teller15mlg@gmail.com>
2022-04-17 16:45:17 +0300

    mining and counter added
    Stone counter doesn't work

---
+++ VikingWalker/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public Text woodCraft;
+
+    private void Update()
+    {
+        woodCraft.text = PlayerPrefs.GetInt("wood").ToString();
+    }
+}
---
+++ VikingWalker/Assets/Scripts/MiningResources.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningResources : MonoBehaviour
+{
+    enum Resource
+    { 
+        wood,
+        stone,
+        gold,
+        diamonds
+    };
+
+    [SerializeField] Resource currentZoneResource;
+    public GameObject resourceObject;
+    [SerializeField, Range(1, 100)] float resourceValue = 100;
+
+    public void MinusValue()
+    {
+        resourceValue = resourceValue - 1;
+        if (resourceValue < 0)
+        {
+            Destroy(resourceObject);
+            Destroy(gameObject);
+        }
+    }
+    public void AddPlayerScore()
+    {
+        int playerResource = PlayerPrefs.GetInt(currentZoneResource.ToString());
+        playerResource = playerResource + 1;
+        PlayerPrefs.SetInt(currentZoneResource.ToString(), playerResource);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.GetComponent<Movement>())
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                MinusValue();
+                AddPlayerScore();
+            }
+        }
+    }
+
+}
+
